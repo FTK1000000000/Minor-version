d1e619d52c711696a7366d62829db15535db293e
ftkbillion <2947702483@qq.com>
2024-08-21 15:03:01 +0800

    移动背包物品_1

--- scripts/ui/inventory.gd
+++ scripts/ui/inventory.gd
@@ -4,25 +4,46 @@ signal opened
 signal closeed
 
 @onready var inventory : Inventory = preload("res://inventory/player_inventory.tres")
+@onready var items_stack_class = preload("res://inventory/items_stack.tscn")
 @onready var slots : Array = $NinePatchRect/GridContainer.get_children()
 
-func update():
-	for i in range(min(inventory.slots.size(), slots.size())):
-		slots[i].update(inventory.slots[i])
+var is_open : bool = false
 
 func _ready():
+	slot_connect()
 	inventory.update.connect(update)
 	update()
 
-var is_open : bool = false
+
+func update():
+	for i in range(min(inventory.slots.size(), slots.size())):
+		var inventory_slot : InventorySlot = inventory.slot[i]
+		
+		if !inventory_slot.item : continue
+		
+		var items_stack : ItemsStack = slots[i].items_stack
+		if !items_stack:
+			items_stack = items_stack_class.instantiate()
+			slots[i].insert(items_stack)
+		
+		items_stack.inventory_slot = inventory_slot
+		items_stack.update()
 
 func open():
 	visible = true
 	is_open = true
 	opened.emit()
 
-
 func close():
 	visible = false
 	is_open = false
 	closeed.emit()
+
+func slot_connect():
+	for slot in slots:
+		var callable = Callable(slot_click)
+		callable = callable.bind(slot)
+		slot.pressed.connect(callable)
+
+func slot_click(slot):
+	pass
--- scripts/ui/slot.gd
+++ scripts/ui/slot.gd
@@ -1,18 +1,11 @@
-extends Panel
+extends Button
 
 @onready var background_sprite : Sprite2D = $Background
-@onready var item_sprite : Sprite2D = $CenterContainer/Panel/Item
-@onready var amount_label : Label = $CenterContainer/Panel/Label
+@onready var container : CenterContainer = $CenterContainer
 
-func update(slot : InventorySlot):
-	if !slot.item:
-		background_sprite.frame = 0
-		item_sprite.visible = false
-		amount_label.visible = false
-	else:
-		background_sprite.frame = 1
-		item_sprite.visible = true
-		item_sprite.texture = slot.item.texture
-		if slot.amount > 1:
-			amount_label.visible = true
-			amount_label.text = str(slot.amount)
+var items_stack : ItemsStack
+
+func insert(isg : ItemsStack):
+	items_stack = isg
+	background_sprite.frame = 1
+	container.add_child(items_stack)
