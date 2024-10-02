32cc75c07b34dd5183ebff3281c5dae3d657c890
ftkbillion <2947702483@qq.com>
2024-10-02 19:17:39 +0800

    可破坏掩体

--- scripts/characters/hurtbox.gd
+++ scripts/characters/hurtbox.gd
@@ -1,10 +1,10 @@
 extends Area2D
 class_name Hurtbox
 
-@onready var parent: CharacterBody2D = get_node("..")
+@onready var parent = get_node("..")
 
 
 func take_damage(dam: int, dir: Vector2, force: int):
 	parent.current_health -= dam
-	parent.velocity += dir * force
+	if !parent is StaticBody2D: parent.velocity += dir * force
 	parent.state_chart.send_event("hurt")
