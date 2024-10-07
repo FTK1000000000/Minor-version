c3af5ee93ccba8fc00930480c9dda5f49ffbafd4
ftkbillion <2947702483@qq.com>
2024-10-08 04:57:51 +0800

    盾类完成，举盾时将期间所受到的碰撞伤害转为减少等量的耐力值。 修复武器攻击导致耐力恢复计时器崩溃bug

--- scripts/weapon/shield.gd
+++ scripts/weapon/shield.gd
@@ -1,11 +1,29 @@
 extends Weapon
+class_name Shield
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var resist_damage: int = 5
+var old_hit_damage = hit_damage
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func special_attack():
+	resist()
+
+
+func resist():
+	if (
+		Input.is_action_just_pressed("attack_special") &&
+		player.current_endurance >= special_attack_consume_endurance
+		):
+			player.current_endurance -= special_attack_consume_endurance
+			if Input.is_action_pressed("attack_special"):
+				hit_damage = resist_damage
+				animation_player.play("resist")
+	
+	elif Input.is_action_just_released("attack_special") && current_charge >= need_charge:
+		hit_damage = old_hit_damage
+		animation_player.play("recover")
+	
+	elif Input.is_action_just_released("attack_special") && current_charge <= need_charge:
+		hit_damage = old_hit_damage
+		animation_player.play("recover")
